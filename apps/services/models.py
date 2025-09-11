cd521e2fbeeb327996199bf1ea03229e0670ab24
Mr-MadnessFan <nsakura5472@gmail.com>
2025-09-11 17:14:54 +0500

    Добавление в сервер.

--- apps/services/models.py
+++ apps/services/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Service(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200, blank=True)
-    icon = models.ImageField(upload_to="services/icons/", blank=True, null=True)
+    icon = models.FileField(upload_to="services/icons/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
