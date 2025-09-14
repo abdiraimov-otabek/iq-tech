58973df8de6ea10318de448b232ec38baf2a6c7b
Afzalbek Mahmudov <afzalbek.777777@gmail.com>
2025-09-14 21:58:51 +0500

    Update views.py

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,8 +1,31 @@
+import os
+import requests
 from rest_framework import generics
-from apps.contact.models import Contact
-from apps.contact.serializers import ContactSerializer
+from .models import Contact
+from .serializers import ContactSerializer
 
 
 class ContactCreateView(generics.CreateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
+    def perform_create(self, serializer):
+        contact = serializer.save()
+
+        BOT_TOKEN = os.getenv("BOT_TOKEN")
+        CHAT_ID = os.getenv("CHAT_ID")
+
+        message = (
+            f"📩 Yangi ariza keldi!\n\n"
+            f"👤 Ism: {contact.name}\n"
+            f"📞 Telefon: {contact.phone_number}\n"
+            f"📝 Habar: {contact.request}"
+        )
+
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        payload = {"chat_id": CHAT_ID, "text": message}
+
+        try:
+            requests.post(url, data=payload)
+        except Exception as e:
+            print("Telegramga yuborishda xato:", e)
