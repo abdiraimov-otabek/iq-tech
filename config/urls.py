a46386c87e16117e41a831ae8423e7b4b5c2b135
Mr-MadnessFan <nsakura5472@gmail.com>
2025-09-18 21:02:40 +0500

    Первый коммит

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -20,3 +22,6 @@ urlpatterns = [
     path("api/v1/blogs/", include("apps.blog.urls")),
     path("api/v1/partners/", include("apps.partners.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
