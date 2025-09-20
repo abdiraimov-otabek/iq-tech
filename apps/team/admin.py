4bebce6d793b89c326437337d9114dd789bcc774
Mr-MadnessFan <nsakura5472@gmail.com>
2025-09-20 12:38:44 +0500

    Первый коммит

--- apps/team/admin.py
+++ apps/team/admin.py
@@ -21,6 +21,6 @@ class RoleAdmin(TranslationAdmin, ModelAdmin):
 class TeamMemberAdmin(TranslationAdmin, ModelAdmin):
     list_display = ("full_name_uz", "email", "slug", "is_active", "created_at", "updated_at")
     search_fields = ("full_name_uz", "email", "role__name")
-    readonly_fields = ("created_at", "updated_at", "slug")
+    readonly_fields = ("created_at", "updated_at")
     prepopulated_fields = {"slug": ("full_name_uz",)}
     formfield_overrides = {models.TextField: {"widget": WysiwygWidget}}
