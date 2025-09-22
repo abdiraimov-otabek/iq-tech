405954fae46fbc3edbd6cbbadc0ecca1cc9b914b
Mr-MadnessFan <nsakura5472@gmail.com>
2025-09-22 14:56:37 +0500

    Первый коммит

--- apps/services/models.py
+++ apps/services/models.py
@@ -6,6 +6,8 @@ class Service(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
     description = models.CharField(max_length=200, blank=True)
     icon = models.FileField(upload_to="services/icons/", blank=True, null=True)
+    file = models.FileField(upload_to="services/3d_file/", blank=True, null=True)
+    colors = models.CharField(max_length=7, default="#000000")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
